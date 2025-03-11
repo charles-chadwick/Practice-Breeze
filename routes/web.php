ddc43f4317a191c2204229acbeff9b8f3263711c
Charles Chadwick <charles@mutuallyassured.com>
2025-03-11 11:50:55 -0700

    Added basic controllers. Will flesh them out later.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AppointmentController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('/users')->group(function() {
+	Route::get("/", [ UserController::class, "index"]);
+});
+Route::prefix('/appointments')->group(function() {
+	Route::get("/", [ AppointmentController::class, "index"]);
 });
