0d284ba82e091ebd8f0bbec089ee8f661c15f098
Charles Chadwick <charles@mutuallyassured.com>
2025-03-12 07:52:49 -0700

    Added in a function to build an array. Not entirely sure this is necessary.

--- app/Enums/UserRole.php
+++ app/Enums/UserRole.php
@@ -4,10 +4,18 @@ namespace App\Enums;
 
 enum UserRole: string {
 	
-	case Admin   = "Admin";
-	case Doctor  = "Doctor";
-	case Nurse   = "Nurse";
-	case Staff   = "Staff";
-	case Patient = "Patient";
+	case Admin  = "Admin";
+	case Doctor = "Doctor";
+	case Nurse  = "Nurse";
+	case Staff  = "Staff";
+	
+	public static function values (): array {
+		
+		return [
+			self::Doctor->value => self::Doctor->name,
+			self::Nurse->value  => self::Nurse->name,
+			self::Staff->value  => self::Staff->name,
+		];
+	}
 	
 }
