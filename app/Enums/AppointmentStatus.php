f54a7395e8dba071811afca76224acc95d5782d4
Charles Chadwick <charles@mutuallyassured.com>
2025-03-14 20:41:38 -0700

    Added Array
    This may not be necessary.

--- app/Enums/AppointmentStatus.php
+++ app/Enums/AppointmentStatus.php
@@ -11,4 +11,16 @@ enum AppointmentStatus: string {
 	case Rescheduled = "Rescheduled";
 	case Completed   = "Completed";
 	
+	public static function asArray (): array {
+		
+		return [
+			self::Available->value   => self::Available->value,
+			self::Scheduled->value   => self::Scheduled->value,
+			self::Pending->value     => self::Pending->value,
+			self::Cancelled->value   => self::Cancelled->value,
+			self::Rescheduled->value => self::Rescheduled->value,
+			self::Completed->value   => self::Completed->value,
+		];
+	}
+	
 }
