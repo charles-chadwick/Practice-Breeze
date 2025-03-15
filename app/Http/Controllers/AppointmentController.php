b32767feddea5a4d56ce9e2564b27cb6b23d28bd
Charles Chadwick <charles@mutuallyassured.com>
2025-03-14 20:41:57 -0700

    Throwing some example logic into the controller

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -5,9 +5,20 @@ namespace App\Http\Controllers;
 use App\Models\Appointment;
 use Illuminate\Http\Request;
 
-class AppointmentController extends Controller
-{
-    public function index() {
-		return Appointment::with('patient')->whereBeforeToday("date_and_time")->get();
+class AppointmentController extends Controller {
+	
+	public function index () {
+		
+		$appointments = Appointment::orderBy(request("order_by", "date_and_time"))
+								   ->with('patient')
+								   ->where(function ($query) {
+									   if (request("patient_id") != "") {
+										   $query->where("patient_id", request("patient_id"));
+									   }
+								   })
+								   ->get();
+		
+		return view("appointments.index", compact("appointments"));
 	}
+	
 }
